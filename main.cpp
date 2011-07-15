f4325e671f4ccb6c0fad205d03ef598bc7281398
Igor <cramur@mail.ru>
2011-07-15 16:29:22 +0400

    Program options

--- main.cpp
+++ main.cpp
@@ -1,28 +1,51 @@
 #include <iostream>
 #include <exception>
+#include <boost/program_options.hpp>
+#include <boost/filesystem.hpp>
 
 #include "InputParser.hpp"
 #include "DutyManager.hpp"
+
+namespace po = boost::program_options;
 
 // TODO logging system на boost::format и с контролем уровня вывода
 
 int main(int argc, char **argv)
 {
-    argc--; argv++;
+    std::string config_fname;
+    int pack; 
 
-    // open input file based on argv.
-    // TODO boost::program_options
-    try {
-        InputParser input(argv[0]);
-    } catch(std::exception &e) {
-        std::cout << e.what() << std::endl;
-        InputParser input("/some/default/input");
+    po::options_description desc("Options");
+    desc.add_options()
+        ("input,i",
+         po::value<std::string>(&config_fname)->default_value("./INPUT.sample"),
+         "Input file to parse with events and persons")
+        ("help,h",
+         "Produce help message")
+        ("pack,n",
+         po::value<int>(&pack)->default_value(4));
+
+    po::variables_map vm;
+    po::store(po::parse_command_line(argc, argv, desc), vm);
+    po::notify(vm);
+
+    if (vm.count("help")) {
+        std::cout << desc << std::endl;
+        return 1;
     }
+
+    boost::filesystem::path config(config_fname);
+    if (!boost::filesystem::exists(config)) {
+        std::cout << "Could not find config file " << config << std::endl; 
+        return -1;
+    }
+
+    InputParser input(config_fname);
 
     std::cout << "Plz lower your expectations." << std::endl;
 
-    DutyManager::test_schedule(4);
+    DutyManager::test_schedule(pack);
 
-
+    return 0;
 }
 
